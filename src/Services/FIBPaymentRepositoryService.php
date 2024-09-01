025dad13ea212a0895a5b1ade313429fec23ea83
Mohamed Jasim Ahmed <34663944+MJAISE@users.noreply.github.com>
2024-09-01 15:09:15 +0300

    feat: implement FIBPaymentRepositoryService
    - Added `createPayment` method to create a new payment record in the database
    - Implemented `getPaymentByFibId` and `getPaymentById` for retrieving payment records by FIB ID and internal ID respectively
    - Developed `getPaymentsByStatus` to fetch payments based on their status
    - Added `updatePaymentStatus` to update the status of a specific payment
    - Implemented `getPurchase` to retrieve the purchase associated with a specific payment
    - Added `updateOrCreateRefund` to handle the creation or update of refund records linked to a payment

--- src/Services/FIBPaymentRepositoryService.php
+++ src/Services/FIBPaymentRepositoryService.php
@@ -1,70 +1,106 @@
 <?php
 
-namespace FirstIraqiBank\FIBPaymentSDK\Services;
+    namespace FirstIraqiBank\FIBPaymentSDK\Services;
 
+    use FirstIraqiBank\FIBPaymentSDK\Model\FibPayment;
+    use FirstIraqiBank\FIBPaymentSDK\Services\Contracts\FIBPaymentRepositoryInterface;
+    use Illuminate\Database\Eloquent\Builder;
+    use Illuminate\Database\Eloquent\Collection;
+    use Illuminate\Database\Eloquent\Model;
+    use Illuminate\Database\Eloquent\ModelNotFoundException;
+    use Illuminate\Support\Facades\Log;
 
-use FirstIraqiBank\FIBPaymentSDK\Model\FibPayment;
-use FirstIraqiBank\FIBPaymentSDK\Services\Contracts\FIBPaymentRepositoryInterface;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\Collection;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
-use Illuminate\Support\Facades\Log;
-
-class FIBPaymentRepositoryService implements FIBPaymentRepositoryInterface
-{
-    public function createPayment(array $paymentData, int $amount): Builder|Model
+    class FIBPaymentRepositoryService implements FIBPaymentRepositoryInterface
     {
-        return  FibPayment::query()->create([
-            'fib_payment_id' => $paymentData['paymentId'],
-            'readable_code' => $paymentData['readableCode'],
-            'personal_app_link' => $paymentData['personalAppLink'],
-            'status' => FibPayment::PENDING,
-            'amount' => $amount,
-            'valid_until' => $paymentData['validUntil'],
-        ]);
-    }
-
-    public function getPaymentByFibId(string $paymentId): Model|Collection|Builder|array|null
-    {
-        $payment = FibPayment::query()->where('fib_payment_id', $paymentId)->firstOrFail();
-
-        if (!$payment) {
-            Log::warning("Payment not found for callback ID: {$paymentId}");
-            throw new ModelNotFoundException("Payment with FIB payment ID {$paymentId} not found");
+        /**
+         * Create a new payment record.
+         *
+         * @param array $paymentData
+         * @param int $amount
+         * @return Builder|Model
+         */
+        public function createPayment(array $paymentData, int $amount)
+        {
+            return FibPayment::query()->create([
+                'fib_payment_id' => $paymentData['paymentId'],
+                'readable_code' => $paymentData['readableCode'],
+                'personal_app_link' => $paymentData['personalAppLink'],
+                'status' => FibPayment::PENDING,
+                'amount' => $amount,
+                'valid_until' => $paymentData['validUntil'],
+            ]);
         }
 
-        return $payment;
+        /**
+         * Retrieve a payment by its FIB payment ID.
+         *
+         * @param string $paymentId
+         * @return Model
+         * @throws ModelNotFoundException
+         */
+        public function getPaymentByFibId(string $paymentId): Model
+        {
+            return FibPayment::query()->where('fib_payment_id', $paymentId)->firstOrFail();
+        }
+
+        /**
+         * Retrieve a payment by its ID.
+         *
+         * @param int $paymentId
+         * @return Model|null
+         */
+        public function getPaymentById(int $paymentId): ?Model
+        {
+            return FibPayment::query()->find($paymentId);
+        }
+
+        /**
+         * Retrieve payments by their status.
+         *
+         * @param array $statuses
+         * @return Collection
+         */
+        public function getPaymentsByStatus(array $statuses): Collection
+        {
+            return FibPayment::query()->whereIn('status', $statuses)
+                ->where('created_at', '<', now()->subMinutes(5))
+                ->get();
+        }
+
+        /**
+         * Update the status of a payment.
+         *
+         * @param string $paymentId
+         * @param string $status
+         * @return void
+         */
+        public function updatePaymentStatus(string $paymentId, string $status): void
+        {
+            // Update the payment status in the database
+            $this->getPaymentByFibId($paymentId)->update(['status' => $status]);
+        }
+
+        /**
+         * Retrieve the purchase associated with a payment.
+         *
+         * @param int $paymentId
+         * @return Model|null
+         */
+        public function getPurchase(int $paymentId): ?Model
+        {
+            return $this->getPaymentById($paymentId)->purchase()->first();
+        }
+
+        /**
+         * Update or create a refund record.
+         *
+         * @param string $paymentId
+         * @param array $refundData
+         * @return void
+         */
+        public function updateOrCreateRefund(string $paymentId, array $refundData): void
+        {
+            $fibPayment = $this->getPaymentByFibId($paymentId);
+            $fibPayment->refund()->updateOrCreate(['payment_id' => $fibPayment->id], $refundData);
+        }
     }
-
-    public function getPaymentById(int $paymentId): Model|Collection|Builder|array|null
-    {
-        return FibPayment::query()->find($paymentId);
-    }
-
-    public function getPaymentsByStatus(array $statuses): Model|Collection|Builder|array|null
-    {
-        return FibPayment::query()->whereIn('status', $statuses)
-            ->where('created_at', '<', now()->subMinutes(5))
-            ->get();
-    }
-
-    public function updatePaymentStatus(string $paymentId, string $status): void
-    {
-        // Update the payment status in the database
-        $this->getPaymentByFibId($paymentId)->update(['status' => $status]);
-    }
-
-    public function getPurchase(int $paymentId)
-    {
-
-        return $this->getPaymentById($paymentId)->purchase()->first();
-    }
-
-    public function updateOrCreateRefund(string $paymentId, array $refundData): void
-    {
-        $fibPayment =  $this->getPaymentByFibId($paymentId);
-        $fibPayment->refund()->updateOrCreate(['payment_id'=>$fibPayment->id], $refundData);
-    }
-
-}
