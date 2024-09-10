075c8bc4a9f8471f6f113d1c857f865cb9900927
Mohamed Jasim Ahmed <34663944+MJAISE@users.noreply.github.com>
2024-09-10 16:23:16 +0300

    test

--- src/config/fib.php
+++ src/config/fib.php
@@ -9,6 +9,7 @@
         'callback' => env('FIB_CALLBACK_URL'),
         'auth_account' => env('FIB_ACCOUNT', 'default'),
 
+        // Default account
         'default' => [
             'client_id' => env('FIB_CLIENT_ID'),
             'secret' => env('FIB_CLIENT_SECRET'),
