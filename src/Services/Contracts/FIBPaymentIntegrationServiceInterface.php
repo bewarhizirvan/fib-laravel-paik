fa2b96b084af1f488da20b2a5b9e51be5931b0f1
bewarhizirvan <61181832+bewarhizirvan@users.noreply.github.com>
2025-04-15 15:53:52 +0300

    Update FIBPaymentIntegrationServiceInterface.php
    Add cid to createPayment

--- src/Services/Contracts/FIBPaymentIntegrationServiceInterface.php
+++ src/Services/Contracts/FIBPaymentIntegrationServiceInterface.php
@@ -4,7 +4,7 @@
 
     interface FIBPaymentIntegrationServiceInterface
     {
-        public function createPayment(int $amount, string|null $callback, string|null $description, string|null $redirectUri);
+        public function createPayment(int $cid, int $amount, string|null $callback, string|null $description, string|null $redirectUri);
         public function checkPaymentStatus($paymentId);
         public function handleCallback(string $paymentId,  string $status);
         public function refund(string $paymentId);
